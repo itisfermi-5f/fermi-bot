6d975166b54c909c53ae60525983ab284ff670d5
Daniele Parmeggiani <dani.parmeggiani@gmail.com>
2016-12-02 22:18:15 +0100

    Added message handling in server.

--- fermi-bot/server.py
+++ fermi-bot/server.py
@@ -30,10 +30,21 @@ class Server:
                 fragment = client.recv(self.buffer_size)
                 if not fragment:
                     break
-                fragments.append(fragment)
+                fragments.append(str(fragment))
             except:
                 client.close()
                 return False
         client.close()
         message = ''.join(fragments)
+        if message == '#-# AUTH #-#':
+            self.handle_authentication(address)
+        else:
+            self.handle_message(address, message)
         return False
+
+    # to be overridden by the Bot
+    def handle_message(self, sender, message):
+        raise NotImplementedError('this method needs to be overridden externally.')
+
+    def handle_authentication(self, sender):
+        raise NotImplementedError('this method needs to be overridden externally.')
